104fff42918107bf5206c580be85032703e49f39
drmagregro <114160314+drmagregro@users.noreply.github.com>
2026-05-20 19:22:53 +0200

    feat: add predict method

--- knn.py
+++ knn.py
@@ -13,3 +13,15 @@ class KNN:
         for index in range(len(a)):
             total = total + (a[index] - b[index]) ** 2
         return total ** 0.5
+    
+    def predict(self, X):
+        predictions = []
+        for row in X:
+            distances = []
+            for point in self.X_train:
+                d = self._distance(point, row)
+                distances.append(d)
+            k_plus_proches = sorted(zip(distances, self.y_train), key=lambda x: x[0])[:self.k]
+            labels = [label for (distance, label) in k_plus_proches]
+            predictions.append(max(labels, key=labels.count))
+        return predictions
